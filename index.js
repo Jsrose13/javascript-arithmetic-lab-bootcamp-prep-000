5f53839f3a2022adcd8bcbb97d305c1ff1f1b34f
JESSICA STEVENS <jsrose13@gmail.com>
2017-08-13 20:05:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function inc(n){
     return n
   }
   function makeInt(n){
-    parseInt('n', 10)
+    parseInt(`n`, 10)
     return n
   }
   function preserveDecimal(n){
-    parseFloat('n')
+    parseFloat(`n`)
     return n 
   }
